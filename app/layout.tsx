673a74f765fdd8f71c1974ff8f38d52134b2bf60
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2024-02-14 13:38:48 +0000

    Add Footer component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <body className={`${inter.className} min-w-[350px]`}>
           <Navbar />
           {children}
+          <Footer />
         </body>
       </ThemeProvider>
     </html>
