eb57b396a6deef0bb7b80834011559bf0fa27cb0
codescalper <msfunbook@gmail.com>
2024-02-19 16:56:37 +0530

    Created text editor for displaying job detaisl

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,20 +1,29 @@
-import {z} from 'zod';
-import { jobTypes, locationTypes } from './job-types';
+import { z } from "zod";
+import { jobTypes, locationTypes } from "./job-types";
 
-const compnayLogoSchema = z.custom<File | undefined>().refine((val) => {
+const compnayLogoSchema = z
+  .custom<File | undefined>()
+  .refine((val) => {
     if (val) {
-      !val || val instanceof File && val.type.startsWith("image/");
+      !val || (val instanceof File && val.type.startsWith("image/"));
     }
-},"Must be an image file").refine(val => !val|| val.size < 1024*1024*2,"Image size must be less than 2MB")
-  
-const numericRegex = (/^\d+$/);
+  }, "Must be an image file")
+  .refine(
+    (val) => !val || val.size < 1024 * 1024 * 2,
+    "Image size must be less than 2MB",
+  );
+
+const numericRegex = /^\d+$/;
 
 const locationSchema = z
   .object({
-    locationType: z.string().min(1, "Required").refine(
-      (value) => locationTypes.includes(value),
-      "Invalid location type",
-    ),
+    locationType: z
+      .string()
+      .min(1, "Required")
+      .refine(
+        (value) => locationTypes.includes(value),
+        "Invalid location type",
+      ),
     location: z.string().max(100).optional(),
   })
   .refine(
@@ -26,31 +35,50 @@ const locationSchema = z
     },
   );
 
-const applicationSchema = z.object({
-  
-    applicationEmail:z.string().email().max(100,"Email must be less than 100 characters").optional().or(z.literal("")),
-    applicationUrl:z.string().url().max(200,"URL must be less than 200 characters").optional().or(z.literal("")),
-}).refine(val => val.applicationEmail || val.applicationUrl,{
-    message:"Either email or URL is required",
-    path:["applicationEmail"]
-})
+const applicationSchema = z
+  .object({
+    applicationEmail: z
+      .string()
+      .email()
+      .max(100, "Email must be less than 100 characters")
+      .optional()
+      .or(z.literal("")),
+    applicationUrl: z
+      .string()
+      .url()
+      .max(200, "URL must be less than 200 characters")
+      .optional()
+      .or(z.literal("")),
+  })
+  .refine((val) => val.applicationEmail || val.applicationUrl, {
+    message: "Either email or URL is required",
+    path: ["applicationEmail"],
+  });
 
-export const createJobSchema = z.object({
-    title:z.string().min(1,"Required").max(100),
-    type:z.string().min(1,"Required").refine(val =>jobTypes.includes(val),"Invalid job type"),
-    companyName:z.string().min(1,"Required").max(100),
-    companyLogo:compnayLogoSchema,
-    description:z.string().max(5000).optional(),
-    salary:z.string().min(1,"Required").max(9,"Number must be less than 9 digits").regex(numericRegex,"Must be a number"),
-
-}).and(applicationSchema)
+export const createJobSchema = z
+  .object({
+    title: z.string().min(1, "Required").max(100),
+    type: z
+      .string()
+      .min(1, "Required")
+      .refine((val) => jobTypes.includes(val), "Invalid job type"),
+    companyName: z.string().min(1, "Required").max(100),
+    companyLogo: compnayLogoSchema,
+    description: z.string().max(5000).optional(),
+    salary: z
+      .string()
+      .min(1, "Required")
+      .max(9, "Number must be less than 9 digits")
+      .regex(numericRegex, "Must be a number"),
+  })
+  .and(applicationSchema);
 
 export const JobFilterSchema = z.object({
-    q:z.string().optional(),
-    type :z.string().optional(),
-    location:z.string().optional(),
-    remote: z.coerce.boolean().optional()
-})
+  q: z.string().optional(),
+  type: z.string().optional(),
+  location: z.string().optional(),
+  remote: z.coerce.boolean().optional(),
+});
 
-export type JobFilterValue = z.infer<typeof JobFilterSchema>
-export type CreateJobValue = z.infer<typeof createJobSchema>
+export type JobFilterValue = z.infer<typeof JobFilterSchema>;
+export type CreateJobValues = z.infer<typeof createJobSchema>;
