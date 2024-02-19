ef4766efa65af43d50b77ac12bf114e11d5be3f0
codescalper <msfunbook@gmail.com>
2024-02-19 16:54:13 +0530

    Added LocationInput component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,11 +8,11 @@ export default function Navbar() {
     <header className="shadow-md shadow-red-500">
       <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
         <Link href="/" className="flex items-center gap-3">
-          <Image src='/rustify.png' width={40} height={40} alt="Rustify logo" />
+          <Image src="/rustify.png" width={40} height={40} alt="Rustify logo" />
           <span className="text-xl font-bold tracking-tight">Rustify</span>
         </Link>
         <Button asChild>
-          <Link href="/jobs/new">Post a job</Link>
+          <Link href="/job/new">Post a job</Link>
         </Button>
       </nav>
     </header>
