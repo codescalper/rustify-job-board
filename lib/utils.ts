b0e5dc46413323314fb457b0ce2b23cc69a9e4ff
codescalper <msfunbook@gmail.com>
2024-02-20 17:05:40 +0530

    Added server action

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { formatDistanceToNowStrict } from "date-fns"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function formatCurrency(amount: number, currency = 'USD') {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency:"USD",
-  }).format(amount)
+export function formatCurrency(amount: number, currency = "USD") {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
 }
 
 export function formatDate(from: Date) {
-  return formatDistanceToNowStrict(from, { addSuffix: true })
+  return formatDistanceToNowStrict(from, { addSuffix: true });
+}
+
+export function toSlug(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[^\w-]+/g, "");
 }
