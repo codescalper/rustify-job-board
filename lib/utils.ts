e751fa533738cc8fa91dc00e6ad0ff81ee637b5e
codescalper <msfunbook@gmail.com>
2024-02-22 20:17:25 +0530

    Added functionality where admin can approve or delete a job request

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { formatDistanceToNowStrict } from "date-fns";
+import { User } from "@clerk/nextjs/server";
+import { UserResource } from "@clerk/types";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -21,4 +23,8 @@ export function toSlug(str: string) {
     .toLowerCase()
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "");
+}
+
+export function isAdmin(user: UserResource | User) {
+  return user.publicMetadata?.role === "admin";
 }
